221d92b5b213fb5ab42d724e0f7a9151ba159d54
Jeremy Foster <jeremymfoster@hotmail.com>
2013-02-24 18:36:31 -0800

    Some caching ideas - not working.

---
+++ Fosol.Common/Caching/CacheItemPriority.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Caching
+{
+    enum CacheItemPriority
+    {
+        Low = 1,
+        BelowNormal,
+        Normal,
+        AboveNormal,
+        High,
+        NotRemovable,
+        Default = Normal
+    }
+}
---
+++ Fosol.Common/Caching/CacheItemRemovedReason.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Caching
+{
+    enum CacheItemRemovedReason
+    {
+        Removed = 1,
+        Expired,
+        Underused,
+        DependancyChanged
+    }
+}
