b961b1ff9d5ab23d4f7c715cd24185644a849034
Jeremy Foster <jeremymfoster@hotmail.com>
2013-02-27 12:45:30 -0800

    Added new NameValueCollection extension methods.  Added new TypeExtension methods.

---
+++ Fosol.Common/Extensions/NameValueCollections/NameValueCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Extensions.NameValueCollections
+{
+    /// <summary>
+    /// Extension methods for NameValueCollection objects.
+    /// </summary>
+    public static class NameValueCollectionExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Determines if the NameValueCollection has the same key and value pairs.
+        /// </summary>
+        /// <param name="obj">Original NameValueCollection object.</param>
+        /// <param name="compare">NameValueCollection to compare with.</param>
+        /// <returns>True if they are equal.</returns>
+        public static bool IsEqual(this NameValueCollection obj, NameValueCollection compare)
+        {
+            return obj.AllKeys.OrderBy(k => k)
+                .SequenceEqual(compare.AllKeys.OrderBy(k => k))
+                && obj.AllKeys.All(k => obj[k] == compare[k]);
+        }
+        #endregion
+    }
+}
---
+++ Fosol.Common/Extensions/Types/TypeExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Extensions.Types
+{
+    /// <summary>
+    /// Extension methods for Type objects.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Checks to see if the Type has a empty constructor.
+        /// </summary>
+        /// <param name="type">Type of object to test.</param>
+        /// <returns>True if the object has an empty constructor.</returns>
+        public static bool HasEmptyConstructor(this Type type)
+        {
+            return (
+                type.GetConstructor(Type.EmptyTypes) != null
+                || type.GetConstructors(BindingFlags.Instance | BindingFlags.Public)
+                    .Any(c => c.GetParameters().All(p => p.IsOptional))
+                );
+        }
+
+        /// <summary>
+        /// Checks to see if the Type only has an empty constructor.
+        /// </summary>
+        /// <param name="type">Type of object to test.</param>
+        /// <returns>True if the object only has an empty constructor.</returns>
+        public static bool OnlyHasEmptyConstructor(this Type type)
+        {
+            return (
+                type.GetConstructor(Type.EmptyTypes) != null
+                && type.GetConstructors(BindingFlags.Instance | BindingFlags.Public).Count() <= 1
+                );
+        }
+
+        /// <summary>
+        /// Checks to see if the Type has a constructor with the specified parameters of the specified Type.
+        /// </summary>
+        /// <param name="type">Type of object to test.</param>
+        /// <param name="paramType">Array of parameter Type to test for.</param>
+        /// <returns>True if the constructor exists.</returns>
+        public static bool HasTypeConstructor(this Type type, params Type[] paramType)
+        {
+            var exists = false;
+
+            foreach (var con in type.GetConstructors(BindingFlags.Instance | BindingFlags.Public))
+            {
+                var cp = con.GetParameters();
+
+                // This constructor does not match.
+                if (cp.Length != paramType.Length)
+                    continue;
+
+                for (var i = 0; i < paramType.Length; i++)
+                {
+                    if (!(cp.Skip(i).SingleOrDefault().ParameterType == paramType[i] != null))
+                        return false;
+                }
+            }
+
+            return exists;
+        }
+        #endregion
+    }
+}
