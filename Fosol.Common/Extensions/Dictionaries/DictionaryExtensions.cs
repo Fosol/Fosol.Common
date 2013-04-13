6c997b41e9c0fff8cbcd01ad056c3263081c5cab
Jeremy Foster <jeremymfoster@hotmail.com>
2013-04-13 14:19:40 -0700

    Adding new DictionaryExtensions.

---
+++ Fosol.Common/Extensions/Dictionaries/DictionaryExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Extensions.Dictionaries
+{
+    /// <summary>
+    /// Dictionary extension methods.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        #region Methods
+        /// <summary>
+        /// Determines if the StringDictionary has the same key and value pairs.
+        /// </summary>
+        /// <param name="obj">Original StringDictionary object.</param>
+        /// <param name="compare">StringDictionary to compare with.</param>
+        /// <returns>True if they are equal.</returns>
+        public static bool IsEqual(this StringDictionary obj, StringDictionary compare)
+        {
+            foreach (string key in obj.Keys)
+            {
+                if (!compare.ContainsKey(key))
+                    return false;
+                else if (!obj[key].Equals(compare[key]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Aggregates the StringDictionary into a query string.
+        /// </summary>
+        /// <param name="obj">StringDictionary object.</param>
+        /// <returns>Query string.</returns>
+        public static string ToQueryString(this StringDictionary obj)
+        {
+            var builder = new StringBuilder();
+            foreach (string key in obj.Keys)
+            {
+                if (builder.Length > 0)
+                    builder.Append("&");
+                builder.Append(key + "=" + obj[key]);
+            }
+            return builder.ToString();
+        }
+        #endregion
+    }
+}
