24e2792ed948f0a95a1b746c0cd6877fac4f2ede
Jeremy Foster <jeremymfoster@hotmail.com>
2014-01-31 13:18:13 -0800

    Added ObjectExtensions.Clone method.

--- Fosol.Common/Extensions/Objects/ObjectExtensions.cs
+++ Fosol.Common/Extensions/Objects/ObjectExtensions.cs
@@ -83,6 +83,31 @@ namespace Fosol.Common.Extensions.Objects
             formatter.Serialize(stream, obj);
             return stream.ToArray();
         }
+
+        /// <summary>
+        /// Deep clones an object into another copy of that object.
+        /// </summary>
+        /// <exception cref="System.ArgumentException">Parameter 'source' must be serializable.</exception>
+        /// <typeparam name="T">Type of the object to clone.</typeparam>
+        /// <param name="source">Source object that will be cloned.</param>
+        /// <returns>A copy of the original 'source' object.</returns>
+        public static T DeepClone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable)
+                throw new ArgumentException("The type of 'source' must be serializable.", "source");
+
+            if (Object.ReferenceEquals(source, null))
+                return default(T);
+
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
         #endregion
     }
 }
