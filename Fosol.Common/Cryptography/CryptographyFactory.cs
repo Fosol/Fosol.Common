f5ba4c7d5aba6849212ffc4cc7f4d42ee3a390a4
Jeremy Foster <jeremymfoster@hotmail.com>
2014-06-07 08:08:09 -0700

    Overloaded encrypt and decrypt methods so that the initialization vector can be managed separately.

--- Fosol.Common/Cryptography/CryptographyFactory.cs
+++ Fosol.Common/Cryptography/CryptographyFactory.cs
@@ -30,10 +30,22 @@ namespace Fosol.Common.Cryptography
         /// Uses Rfc2898DeriveBytes object to generate an algorithm hash.
         /// </summary>
         /// <param name="data">Data to be encrypted.</param>
-        /// <param name="key">Key used to encrypt data.</param>
+        /// <param name="password">Password used to encrypt data.</param>
         /// <param name="salt">Salt to use for encrypting data.</param>
         /// <returns>Encrypted data.</returns>
-        public abstract byte[] Encrypt(byte[] data, string key, byte[] salt = null);
+        public abstract byte[] Encrypt(byte[] data, string password, byte[] salt);
+
+        /// <summary>
+        /// Encrypt the data.
+        /// Uses Rfc2898DeriveBytes object to generate an algorithm hash.
+        /// </summary>
+        /// <param name="data">Data to be encrypted.</param>
+        /// <param name="password">Password used to encrypt data.</param>
+        /// <param name="iv">Algorithm initialization vector.</param>
+        /// <param name="keySize">Size in bytes of the key.</param>
+        /// <param name="ivSize">Size in bytes of the initialization vector.</param>
+        /// <returns>Encrypted data.</returns>
+        public abstract byte[] Encrypt(byte[] data, string password, byte[] salt, out byte[] iv, int keySize = 32, int ivSize = 16);
 
         /// <summary>
         /// Encrypt the data.
@@ -59,10 +71,22 @@ namespace Fosol.Common.Cryptography
         /// Uses Rfc2898DeriveBytes object to generate an algorithm hash.
         /// </summary>
         /// <param name="data">Data to be decrypted.</param>
-        /// <param name="key">Key used to decrypt data.</param>
+        /// <param name="password">Password used to decrypt data.</param>
         /// <param name="salt">Salt to use for decrypting data.</param>
         /// <returns>Decrypted data.</returns>
-        public abstract byte[] Decrypt(byte[] data, string key, byte[] salt = null);
+        public abstract byte[] Decrypt(byte[] data, string password, byte[] salt);
+
+        /// <summary>
+        /// Decrypt the data.
+        /// Uses Rfc2898DeriveBytes object to generate an algorithm hash.
+        /// </summary>
+        /// <param name="data">Data to be decrypted.</param>
+        /// <param name="password">Password used to decrypt data.</param>
+        /// <param name="salt">Salt to use for decrypting data.</param>
+        /// <param name="iv">Algorithm initialization vector.</param>
+        /// <param name="keySize">Size in bytes of the key.</param>
+        /// <returns>Decrypted data.</returns>
+        public abstract byte[] Decrypt(byte[] data, string password, byte[] salt, byte[] iv, int keySize = 32);
 
         /// <summary>
         /// Decrypt the data.
