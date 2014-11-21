1ee7198fc09bb7bbd299ab12d1c479a2cef60a74
Jeremy Foster <jeremymfoster@hotmail.com>
2014-11-21 11:20:25 -0800

    Adding features to PasswordUtility.

---
+++ Fosol.Common/Utilities/PasswordStrength.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Utilities
+{
+    /// <summary>
+    /// PasswordStrength provides a way to define how strong a given password is.
+    /// </summary>
+    public sealed class PasswordStrength
+    {
+        #region Variables
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// get - Number of characters.
+        /// </summary>
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// get - Number of nonalphabetic characters.
+        /// </summary>
+        public int NonalphaCharacters { get; private set; }
+
+        /// <summary>
+        /// get - Number of nonalphabetic and nondigit characters.
+        /// </summary>
+        public int NonalphaDigitCharacters { get; private set; }
+
+        /// <summary>
+        /// get - Number of alphabetic characters.
+        /// </summary>
+        public int AlphaCharacters { get; private set; }
+
+        /// <summary>
+        /// get - Number of digit characters.
+        /// </summary>
+        public int DigitCharacters { get; private set; }
+
+        /// <summary>
+        /// get - Number of uppercase characters.
+        /// </summary>
+        public int UppercaseCharacters { get; private set; }
+
+        /// <summary>
+        /// get - Number of lowercase characters.
+        /// </summary>
+        public int LowercaseCharacters { get; private set; }
+        #endregion
+
+        #region Constructors
+        internal PasswordStrength()
+        {
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Evaluates the specified password and returns a new instance of a PasswordStrength object which provides the strength of the password.
+        /// </summary>
+        /// <param name="password">Password to evaluate.</param>
+        /// <returns>New instance of a PasswordStrength object.</returns>
+        public static PasswordStrength Evaluate(string password)
+        {
+            Fosol.Common.Validation.Assert.IsNotNullOrEmpty(password, "password");
+
+            int nonalpha = 0;
+            int nonalphadigit = 0;
+            int alpha = 0;
+            int digit = 0;
+            int upper = 0;
+            int lower = 0;
+
+            // Check each character and increment the various strength indicators.
+            foreach (var c in password.ToArray())
+            {
+                if (Char.IsLetter(c))
+                    alpha++;
+                else
+                    nonalpha++;
+                if (Char.IsDigit(c))
+                    digit++;
+                if (Char.IsLower(c))
+                    lower++;
+                if (Char.IsUpper(c))
+                    upper++;
+                if (!Char.IsLetterOrDigit(c))
+                    nonalphadigit++;
+            }
+
+            return new PasswordStrength()
+            {
+                Length = password.Length,
+                NonalphaCharacters = nonalpha,
+                NonalphaDigitCharacters = nonalphadigit,
+                AlphaCharacters = alpha,
+                DigitCharacters = digit,
+                UppercaseCharacters = upper,
+                LowercaseCharacters = lower
+            };
+        }
+        #endregion
+
+        #region Operators
+        #endregion
+
+        #region Events
+        #endregion
+    }
+}
---
+++ Fosol.Common/Utilities/SqlUtility.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Fosol.Common.Utilities
+{
+    /// <summary>
+    /// SqlUtility provides a handful of helpful methods to interact with databases.
+    /// </summary>
+    public static class SqlUtility
+    {
+        #region Variables
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Execute the script specified at the path.
+        /// </summary>
+        /// <param name="connection">Connection to the database.</param>
+        /// <param name="path">Path to the SQL script file.</param>
+        /// <param name="commandTimeout">The time in seconds to wait for the command to execute.  '0' means no timeout.</param>
+        /// <returns>The number of rows affected by the script.</returns>
+        public static int ExecuteScript(System.Data.IDbConnection connection, string path, int commandTimeout = 0)
+        {
+            Fosol.Common.Validation.Assert.IsNotNull(connection, "connection");
+            Fosol.Common.Validation.Assert.IsNotNullOrEmpty(path, "path");
+
+            var script = File.ReadAllText(path);
+
+            // Need to parse the script for each individual 'GO' statement because a DbCommand cannot execute that keyword.
+            var steps = Regex.Split(script, @"^\s*GO\s*$", RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Where(s => s != String.Empty);
+            int result = 0;
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandTimeout = commandTimeout;
+
+            foreach (var sql in steps)
+            {
+                cmd.CommandText = sql;
+                result += cmd.ExecuteNonQuery();
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region Operators
+        #endregion
+
+        #region Events
+        #endregion
+    }
+}
